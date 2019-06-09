057e3a3632e37db45a743144dee879a2ebed1056
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2019-06-10 00:58:58 +0530

    Nitpicks, `-wait' flag.

--- doc.go
+++ doc.go
@@ -38,25 +38,25 @@ import (
 
 func usage() {
 	fmt.Fprintf(os.Stderr,
-		"netflix-passwd-rotate: A CLI for rotating passwords on Netflix.\n"+
+		"netflix-passwd-rotate: A CLI for rotating passwords on Netflix.         \n"+
 			"\nUsage:\n"+
-			"  netflix-passwd-rotate -username {user} -old-password {old-pw}    \n"+
-			"                        -new-password {new-pw} -auto-generate      \n"+
-			"                        -max-len {M} -num-digits {D} -no-upper     \n"+
-			"                        -num-symbols {S} -allow-repeat -no-color   \n"+
-			"                        -dev-logout -tmp-dir {tmp} -out-file {out} \n"+
-			"                        -exec-path {bin} -wait-sec {W}             \n"+
+			"  netflix-passwd-rotate -username {user} -old-password {old-pw}     \n"+
+			"                        -new-password {new-pw} -auto-generate       \n"+
+			"                        -max-len {M} -num-digits {D} -no-upper      \n"+
+			"                        -num-symbols {S} -allow-repeat -no-color    \n"+
+			"                        -dev-logout -tmp-dir {tmp} -out-file {out}  \n"+
+			"                        -exec-path {bin} -wait-sec {W}              \n"+
 			"\nArguments:\n"+
-			"  -username             Netflix username to login with.     \n"+
-			"  -old-password         The current Netflix password.       \n"+
-			"  -new-password         The new Netflix password.           \n"+
-			"  -auto-generate        Generate a Netflix password.        \n"+
-			"  -no-color             Disable colored output.             \n"+
-			"  -dev-logout           Force log-out from all devices.     \n"+
-			"  -tmp-dir              Temporary directory for user data.  \n"+
-			"  -out-file             Write the new password to file.     \n"+
-			"  -exec-path            Path to the `google-chrome' binary. \n"+
-			"  -wait-sec             Time to wait for the operation.     \n"+
+			"  -username             Netflix username to login with.             \n"+
+			"  -old-password         The current Netflix password.               \n"+
+			"  -new-password         The new Netflix password.                   \n"+
+			"  -auto-generate        Generate a Netflix password.                \n"+
+			"  -no-color             Disable colored output.                     \n"+
+			"  -dev-logout           Force log-out from all devices.             \n"+
+			"  -tmp-dir              Temporary directory for user data.          \n"+
+			"  -out-file             Write the new password to file.             \n"+
+			"  -exec-path            Path to the `google-chrome' binary.         \n"+
+			"  -wait-sec             Time to wait for the operation.             \n"+
 			"\nOther:\n"+
 			"  For -auto-generate:\n"+
 			"    -max-len            The maximum length of the password.         \n"+
